256dc331abcf744401338977d644bbbb4414af78
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-03-15 08:36:11 +0700

    commit again java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   
         public static void main(String[] args) {
-            System.out.print("Hello World!"); // Display the string.
+            System.out.println("Hello World!"); // Display the string.
            
         }
 }
